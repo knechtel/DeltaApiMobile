ec76e880581d3ab497d3fd25da2d9adca0ebda67
maiquelknechtel <maiquelknechtel@maiquelknechtels-MacBook-Pro.local>
2020-07-06 19:33:24 -0300

    Adicionando aparelho data access object

--- src/main/java/com/delta/delta/dao/AparelhoDAO.java
+++ src/main/java/com/delta/delta/dao/AparelhoDAO.java
@@ -1,9 +1,14 @@
 package com.delta.delta.dao;
 
 import com.delta.delta.bean.Aparelho;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AparelhoDAO extends CrudRepository<Aparelho, Long> {
+    @Query("select a.idCliente,a.nome from Aparelho a")
+    public List<Aparelho>findAllB();
 }
